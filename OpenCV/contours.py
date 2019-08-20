1f6bfefb51bca719d80056818c82a746543636de
Vishal Varghese <vishalvarghese46@gmail.com>
2019-08-20 23:20:23 +0100

    contour server

--- OpenCV/contours.py
+++ OpenCV/contours.py
@@ -1,7 +1,7 @@
 import numpy as np
 import cv2
 
-img = cv2.imread(r'E:\Python\OpenCVLearning\OpenCV\images\logo.jpg', 1)
+img = cv2.imread(r'E:\Python\OpenCVLearning\OpenCV\images\shapes.jpg', 1)
 grey_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
 ret, thresh = cv2.threshold(grey_img, 127, 255, 0)
