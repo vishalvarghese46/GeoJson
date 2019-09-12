5b35b119366b7d2703320a0e93f0a5e33698a903
Vishal Varghese <vishalvarghese46@gmail.com>
2019-09-12 23:27:54 +0100

    All lines detected

--- OpenCV/HoughCircleTransform/houghLine.py
+++ OpenCV/HoughCircleTransform/houghLine.py
@@ -1,23 +1,17 @@
 import cv2
 import numpy as np
 
-img = cv2.imread(r'E:\Python\OpenCVLearning\OpenCV\images\track.jpg', 1)
+img = cv2.imread(r'E:\Python\OpenCVLearning\OpenCV\images\lines.jpg', 1)
+
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,50,150,apertureSize = 3)
+edges = cv2.Canny(gray,100,200,apertureSize = 3)
+minLineLength = 30
+maxLineGap = 10
 
-lines = cv2.HoughLines(edges,1,np.pi/180,200)
-for rho,theta in lines[0]:
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a*rho
-    y0 = b*rho
-    x1 = int(x0 + 1000*(-b))
-    y1 = int(y0 + 1000*(a))
-    x2 = int(x0 - 1000*(-b))
-    y2 = int(y0 - 1000*(a))
+lines = cv2.HoughLinesP(edges,1,np.pi/180,15,minLineLength,maxLineGap)
+for x in range(0, len(lines)):
+    for x1,y1,x2,y2 in lines[x]:
+        cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 
-    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
-
-cv2.imshow("Dude", img)
+cv2.imshow('hough',img)
 cv2.waitKey(0)
-cv2.destroyAllWindows()
