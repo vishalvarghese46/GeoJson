2735b094ad301c87a3b6e5d78a6556caf78251da
Vishal Varghese <vishalvarghese46@gmail.com>
2019-09-13 00:25:48 +0100

    Adding some updates

--- OpenCV/HoughCircleTransform/first.py
+++ OpenCV/HoughCircleTransform/first.py
@@ -3,25 +3,26 @@ import numpy as np
 import time
 
 start = int(round(time.time() * 1000))
-img = cv2.imread(r'E:\Python\OpenCVLearning\OpenCV\images\coins.jpg', 0)
-img = cv2.medianBlur(img, 5)
-cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+img = cv2.imread(r'E:\Python\OpenCVLearning\OpenCV\images\shapes.jpg', 1)
+image = cv2.medianBlur(img, 5)
+grayImage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-circle = cv2.HoughCircles(img, cv2.HOUGH_GRADIENT, 1, 120, param1=50,
-                                                param2=30, minRadius=0, maxRadius=0)
+circles = cv2.HoughCircles(grayImage, cv2.HOUGH_GRADIENT, 1, 120,
+              param1=50,
+              param2=30,
+              minRadius=0,
+              maxRadius=0)
 
-circle = np.uint16(np.around(circle))
-for i in circle[0,:]:
-    # draw the outer circle
-    cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
-    # draw the center of the circle
-    #cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+circles = np.uint16(np.around(circles))
+for i in circles[0,:]:
+    cv2.circle(img,(i[0],i[1]),i[2],(0,255,0),2)
+    cv2.circle(img,(i[0],i[1]),2,(0,0,255),3)
 
 end = int(round(time.time() * 1000))
 print(f'Time in Millisec: {end-start}')
 print(f'Time in Sec: {(end-start)/1000}')
 
-cv2.imshow("Dude", cimg)
+cv2.imshow("Dude", img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
 
--- OpenCV/HoughCircleTransform/houghLine.py
+++ OpenCV/HoughCircleTransform/houghLine.py
@@ -3,14 +3,14 @@ import numpy as np
 
 img = cv2.imread(r'E:\Python\OpenCVLearning\OpenCV\images\lines.jpg', 1)
 
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,100,200,apertureSize = 3)
+grayImage = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cannyEdge = cv2.Canny(grayImage,100,200,apertureSize = 3)
 minLineLength = 30
 maxLineGap = 10
 
-lines = cv2.HoughLinesP(edges,1,np.pi/180,15,minLineLength,maxLineGap)
-for x in range(0, len(lines)):
-    for x1,y1,x2,y2 in lines[x]:
+lines = cv2.HoughLinesP(cannyEdge,1,np.pi/180,15,minLineLength,maxLineGap)
+for line in lines:
+    for x1,y1,x2,y2 in line:
         cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 
 cv2.imshow('hough',img)
